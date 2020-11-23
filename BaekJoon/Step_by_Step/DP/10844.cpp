ae4a37cc193bea4c480f3faa4eb678338ede0d7d
WonSang98 <cdsywon@kookmin.ac.kr>
2020-11-23 19:18:37 +0900

    Solving 'Problem 1463' - 2nd Try

--- BaekJoon/Step_by_Step/DP/10844.cpp
+++ BaekJoon/Step_by_Step/DP/10844.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n;
-	int answer = 9;
-	for(int i=1; i<n; i++){
-		answer *= 2;
-		answer -= i;
+int n;
+int answer = 0;
+int stair[10] = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int dp[10];
+
+void solve(){
+	for(int i=2; i<=n; i++){
+		for(int j=0; j<10; j++){
+			if(j == 0) dp[0] = stair[1];
+			else if(j == 9) dp[9] = stair[8];
+			else dp[j] = stair[j-1] + stair[j+1];
+			dp[j] %= 1000000000;
+		}
+		for(int k=0; k<10; k++){
+			stair[k] = dp[k];
+		}
+	}
+	for(int j=0; j<10; j++){
+		answer += stair[j];
 		answer %= 1000000000;
 	}
 	cout << answer;
+}
+
+int main() {
+	cin >> n;
+	solve();
 	return 0;
 }
