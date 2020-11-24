8582b326de95801e19f3af8aa7641b0ba87366b5
WonSang98 <cdsywon@kookmin.ac.kr>
2020-11-24 18:12:47 +0900

    Solving 'Problem 11053' - 1st Try
    Wrong answer.
    because, I had to print that biggest number in 'dp' , but I printed 'dp[n-1]'.

---
+++ BaekJoon/Step_by_Step/DP/11053.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[1000];
+int dp[1000] = {1, };
+
+int max(int a, int b){
+	return a > b ? a: b;
+}
+
+void init(){
+	cin >> n;
+	for(int i=0; i<1000; i++){
+		cin >> arr[i];
+	}
+}
+
+void solve(){
+	dp[0] = 1;
+	for(int i=1; i<n; i++){
+		dp[i] = 1;
+		for(int j=0; j<i; j++){
+			if(arr[i] > arr[j]){
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+	cout << dp[n-1];
+}
+
+
+int main() {
+	init();
+	solve();
+}
